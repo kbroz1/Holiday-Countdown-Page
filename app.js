edf489a6cee54ff68035a84b4da9955ebdee7e90
kbroz1 <kmbroz@gmail.com>
2021-04-29 21:14:06 -0600

    added date,time,day of week

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-confirm ("Do you like fruit?");
-prompt ("What is your favorite type of fruit?")
-// browser objects
+// confirm ("Do you like fruit?");
+// prompt ("What is your favorite type of fruit?")
+// // browser objects
+
+//this gets the exact time//
+var currentTime= new Date();
+
+var time= document.getElementById("cTime");
+time.innerHTML= currentTime.toLocaleTimeString();
+
+//this gets the date//
+
+var date= document.getElementById("cDate");
+date.innerHTML= currentTime.toLocaleDateString();
+
+//this gets the days of the week//
+var weekDays= new Array("Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun");
+var dow= document.getElementById("dayOfWeek");
+dow.innerHTML= weekDays[currentTime.getDay()];
